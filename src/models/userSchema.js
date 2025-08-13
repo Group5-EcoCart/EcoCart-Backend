f44ecf7522b2c1ac837fae4a08f9f7703282f27b
joseph200536 <joseph200536@gmail.com>
2025-08-14 01:10:16 +0530

    userAuth Completed

---
+++ src/models/userSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+    {
+        email:{type:String,required:true},
+        role:{type:String,required:true},
+        password:{type:String,required:true},
+    },
+    {timestamps:true}
+);
+userSchema.pre("save",async function(next){
+    if(!this.isModified){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+});
+
+const UserModel = mongoose.model("User",userSchema);
+
+export default UserModel;
