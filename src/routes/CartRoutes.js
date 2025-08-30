ad175836412f9a49b413acbf16e9c7d01aab7e53
Karthik G <ganesankarthiiik@gmail.com>
2025-08-30 12:34:14 +0530

    CartRoutes.js

--- src/routes/CartRoutes.js
+++ src/routes/CartRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { addToCart, getCart } from "/controllers/CartController.js";
-import { protect } from "../middleware/AuthMiddleware.js";
+import { protect, isBuyer } from "../middleware/AuthMiddleware.js";
 
 const router = express.Router();
 
-router.post("/add", protect, addToCart);
-router.get("/", protect, getCart);
+
+router.post("/add", protect, isBuyer, addToCart);
+
+
+router.get("/", protect, isBuyer, getCart);
 
 export default router;
